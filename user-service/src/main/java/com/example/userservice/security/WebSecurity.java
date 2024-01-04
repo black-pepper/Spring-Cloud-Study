cdb123d8c0b4908d4546aca31fefb786a8805892
Uju <56465463+black-pepper@users.noreply.github.com>
2024-01-04 15:51:37 +0900

    Add Spring Security

---
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -0,0 +1,24 @@
+package com.example.userservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf->csrf.disable());
+        http.authorizeHttpRequests(request->{
+            request.requestMatchers(antMatcher("/users/**")).permitAll();
+            request.requestMatchers(antMatcher("/h2-console/**")).permitAll();
+        });
+        http.headers(headers->headers.frameOptions(frameOptions->frameOptions.disable()));
+        return http.build();
+    }
+}
