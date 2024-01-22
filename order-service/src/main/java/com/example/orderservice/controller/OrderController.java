01cebdc44520eaafd8a6022b91a331754b7bbd60
Uju <56465463+black-pepper@users.noreply.github.com>
2024-01-22 14:24:11 +0900

    Microservice의 분산 추적

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -8,6 +8,7 @@ import com.example.orderservice.service.OrderService;
 import com.example.orderservice.vo.RequestOrder;
 import com.example.orderservice.vo.ResponseOrder;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
@@ -22,6 +23,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/order-service")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
     private final OrderService orderService;
     private final Environment env;
@@ -36,6 +38,7 @@ public class OrderController {
     @PostMapping("/{userId}/orders")
     public ResponseEntity<ResponseOrder> createOrder(@PathVariable("userId") String userId,
                                                      @RequestBody RequestOrder orderDetails) {
+        log.info("Before add order data");
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
@@ -56,15 +59,18 @@ public class OrderController {
 //        orderProducer.send("orders", orderDto);
 //
 //        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
+        log.info("After Added order data");
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
     }
 
     @GetMapping("/{userId}/orders")
     public ResponseEntity<List<ResponseOrder>> getOrder(@PathVariable("userId") String userId) {
+        log.info("Before retrieve order data");
         Iterable<OrderEntity> orderList = orderService.getOrdersByUserId(userId);
         List<ResponseOrder> result = new ArrayList<>();
         ModelMapper mapper = new ModelMapper();
         orderList.forEach(v->result.add(mapper.map(v, ResponseOrder.class)));
+        log.info("Add retrieved order data");
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
